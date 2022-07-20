4026247a0c680040f94add7e230553f482751a22
JamKyu <jamkyu@pm.me>
2022-07-20 16:39:25 -0400

    Initial Commit

---
+++ components/SideBar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  HomeIcon,
+  HashtagIcon,
+  BellIcon,
+  MailIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+import SideBarRow from "./SideBarRow";
+
+function SideBar() {
+  return (
+    <div className="flex flex-col col-span-2 items-center px-4 lg:items-start">
+      <img
+        className="h-6 w-6 m-3 lg:h-10 lg:w-10"
+        src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"
+        alt=""
+      />
+      <SideBarRow Icon={HomeIcon} title="Home" />
+      <SideBarRow Icon={HashtagIcon} title="Explore" />
+      <SideBarRow Icon={BellIcon} title="Notifications" />
+      <SideBarRow Icon={MailIcon} title="Messages" />
+      <SideBarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SideBarRow Icon={CollectionIcon} title="Lists" />
+      <SideBarRow Icon={UserIcon} title="Sign In" />
+      <SideBarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  );
+}
+
+export default SideBar;
